3872f8e4b73e8d70b23019a19ac86731cae7e080
uladzimir <01myit@gmail.com>
2023-07-17 20:41:45 +0300

    modal animate without library

---
+++ src/Modal.js
@@ -0,0 +1,66 @@
+import {useState} from 'react';
+import {Container} from 'react-bootstrap';
+import './App.css'
+const hiddenStyles = {
+  opacity: 0,
+  visibility: 'hidden',
+  transition: 'all 700ms ease-in-out'
+}
+const showStyles = {
+  opacity: 1,
+  visibility: 'visible',
+  transition: `all 700ms ease-in-out`
+}
+const ModalWindow = (props) => {
+  console.log(props.show)
+  return (
+        <div className="modal mt-5 d-block" style={props.show ? showStyles :  hiddenStyles}>
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h5 className="modal-title">Typical modal window</h5>
+                  <button onClick={() => props.setShow(false)} type="button" className="btn-close" aria-label="Close"></button>
+                </div>
+                <div className="modal-body">
+                  <p>Modal body content</p>
+                </div>
+                <div className="modal-footer">
+            <button onClick={() => {
+              props.setShow(false)
+              props.setBtnShow(true)
+            }} type="button" className="btn btn-secondary">Close</button>
+            <button onClick={() => {
+              props.setShow(false)
+              props.setBtnShow(true)
+            }} type="button" className="btn btn-primary">Save changes</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        )
+}
+
+function Modal() {
+  const [show, setShow] = useState(false)
+  const [btnShow, setBtnShow] = useState(true)
+
+    return (
+        <Container>
+        <ModalWindow
+          show={show}
+          setShow={setShow}
+          setBtnShow={setBtnShow}
+        />
+        { btnShow ? <button 
+          type="button" 
+          className="btn btn-warning mt-5"
+          onClick={() => {
+            setShow(true)
+            setBtnShow(false)
+          }}>Open Modal</button> : null
+        }
+        </Container>
+    );
+}
+
+export default Modal;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
 // import App from './App'
-import CssTransition from './CssTransition'
+// import CssTransition from './CssTransition'
+import Modal from './Modal'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 createRoot(document.getElementById('root')).render(
   <>
     {/* <App /> */}
-    < CssTransition/>
+    {/* < CssTransition /> */}
+    < Modal />
   </>
 )
 
