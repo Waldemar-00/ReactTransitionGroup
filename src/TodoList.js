549e4cf7a17f7e55f96f99a25a7b53db3f1223b9
uladzimir <01myit@gmail.com>
2023-07-19 21:02:00 +0300

    added uuidv4, realised methods add and delete li of list, used Fragment of React without any errors

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,31 +1,34 @@
-import { useState, useEffect } from 'react'
-
+import { useState, useEffect, Fragment } from 'react'
+import {v4} from 'uuid'
 import './App.css' 
 function TodoList() {
-  const [array, setArray] = useState([{ text: 'hello' }, { text: 'World' }])
+  const [array, setArray] = useState([{ text: 'hello', id: v4() }, { text: 'World', id: v4() }])
   const [input, setInput] = useState('')
   useEffect(() => {
-
-  }, [])
+    console.log(array)
+  }, [array])
   function onChange(e) {
     setInput(e.target.value)
   }
   function onAdd() {
-    setArray([...array, {text: input}])
+    setArray([...array, { text: input, id: v4() }])
+  }
+  function onDeleteInput(e) {
+    setArray(array.filter(arr => arr.id !== e.target.id))
   }
   return (
     <>
       <div className="wrap">
       <ul className="ul">
-        {
-          array.map(({text}, index) => {
-            return (
-              <>
-                <li key={index}><span>&#10008;</span>{text}</li>
-              </>
-            )
-        })
-      }
+          {
+            array.map(({text, id}) => {
+              return (
+                <Fragment key={id}>
+                  <li id={id} onClick={onDeleteInput}>&#10008; {text}</li>
+                </Fragment>
+              )
+            })
+          }
       </ul>
       <div className="input">
         <label htmlFor="input" >Add to the list</label>
